309ef20c6328c0dada2ee1613b99aae0c0d5a1d9
jp <jrgnpst@gmail.com>
2023-12-30 00:06:18 +0200

    lambda fix

--- feedback/models.py
+++ feedback/models.py
@@ -1,17 +1,24 @@
 from django.db import models
 import uuid
-from entry.views import User
+from entry.models import User
 from datetime import timedelta
 from django.utils import timezone
 from .constants import DEFAULT_FEEDBACK_WINDOW, DEFAULT_DATA_RETENTION_PERIOD
 
+
+# For lambda
+def default_feedback_send_window_end():
+    return timezone.now() + timedelta(days=DEFAULT_FEEDBACK_WINDOW)
+
+def default_data_retention_end_time():
+    return timezone.now() + timedelta(days=DEFAULT_DATA_RETENTION_PERIOD)
 
 # basis for extensions and user feature RBAC logic
 # When initializing a new FeedbackRound, 1 option:
 # feedback_window = specific_feedback_window or GlobalSettings.objects.get(name="default_feedback_window").value
 
 class GlobalSettings(models.Model):
-    glbal_feedback_send_window_end = models.IntegerField(default=DEFAULT_FEEDBACK_WINDOW)
+    global_feedback_send_window_end = models.IntegerField(default=DEFAULT_FEEDBACK_WINDOW)
     global_data_retention_end_time = models.IntegerField(default=DEFAULT_DATA_RETENTION_PERIOD)
    
 
@@ -20,8 +27,8 @@ class FeedbackRound(models.Model):
     name = models.CharField(max_length=150)
     start_time = models.DateTimeField(auto_now_add=True)
     feedback_round_code = models.CharField(max_length=25, default=uuid.uuid4, unique=True)
-    feedback_send_window_end = models.DateTimeField(default=lambda: timezone.now() + timedelta(days=DEFAULT_FEEDBACK_WINDOW))
-    data_retention_end_time = models.DateTimeField(default=lambda: timezone.now() + timedelta(days=DEFAULT_DATA_RETENTION_PERIOD))
+    feedback_send_window_end = models.DateTimeField(default=default_feedback_send_window_end)  # call from separate funcs for lambda
+    data_retention_end_time = models.DateTimeField(default=default_data_retention_end_time)  # call from separate funcs for lambda
 
     @classmethod
     def can_initiate_new_round(cls, employer_id):
