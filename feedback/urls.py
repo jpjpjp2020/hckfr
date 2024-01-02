bf33833820c25202f20cec7123e40beeffa1f623
jp <jrgnpst@gmail.com>
2024-01-02 15:00:16 +0200

    edb tools rough paths and views

--- feedback/urls.py
+++ feedback/urls.py
@@ -4,7 +4,12 @@ from . import views
 app_name = 'feedback'
 
 urlpatterns = [
+    # dashboards
     path('wdashboard/', views.worker_dashboard, name='worker_dashboard'),
     path('edashboard/', views.employer_dashboard, name='employer_dashboard'),
     path('odashboard/', views.oversight_dashboard, name='oversight_dashboard'),
+    # employer tools
+    path('edashboard/new_round/', views.new_feedback_round, name='new_feedback_round'),
+    path('edashboard/all_rounds/', views.all_active_rounds, name='all_active_rounds'),
+    path('edashboard/guides/', views.employer_guides, name='employer_guides'),
 ]
--- feedback/views.py
+++ feedback/views.py
@@ -10,15 +10,15 @@ from entry.decorators import role_required
 
 @role_required('worker', redirect_url='entry:worker_login')
 def worker_dashboard(request):
-    return render(request, 'feedback/worker_dashboard.html')
+    return render(request, 'dashboard/worker_dashboard.html')
 
 @role_required('employer', redirect_url='entry:employer_login')
 def employer_dashboard(request):
-    return render(request, 'feedback/employer_dashboard.html')
+    return render(request, 'dashboard/employer_dashboard.html')
 
 @role_required('oversight', redirect_url='entry:oversight_login')
 def oversight_dashboard(request):
-    return render(request, 'feedback/oversight_dashboard.html')
+    return render(request, 'dashboard/oversight_dashboard.html')
 
 # custom
 
@@ -27,3 +27,17 @@ class CustomLogoutView(View):
         logout(request)
         # if needed - branch conditionally
         return HttpResponseRedirect('/')
+    
+# employer dashboard tools
+    
+@role_required('employer', redirect_url='entry:employer_login')
+def new_feedback_round(request):
+    return render(request, 'initial/new_feedback_round.html')
+
+@role_required('employer', redirect_url='entry:employer_login')
+def all_active_rounds(request):
+    return render(request, 'active/all_active_rounds.html')
+
+@role_required('employer', redirect_url='entry:employer_login')
+def employer_guides(request):
+    return render(request, 'guides/employer_guides.html')
